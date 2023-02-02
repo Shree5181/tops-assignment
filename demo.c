998e0df635da701905155fda7988a13d7a5a37eb
Shree5181 <123551523+Shree5181@users.noreply.github.com>
2023-02-02 15:11:47 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int name;
+    int birthdate;
+    int age;  
+    int address; 
+    printf("name: shruti");
+    printf("birthdate: 30-3-2003");
+    printf("age: 20");
+    printf("address: 81,asvin sos khodiyar nagar rode,varachha");
+    return 0;
+    }
